f3992cc7a89b49669669f6a57681efc7dd8e2560
Yiqiu-Zhang <ezhang13617@gmail.com>
2022-09-07 17:12:43 +0800

    test

--- test.py
+++ test.py
@@ -3,4 +3,6 @@ import os
 
 base_folder = os.path.expanduser('~')+'/AF_data_clear/'
 
-shutil.make_archive(base_folder+ 'distillation_dataset/pkl/pkl_10'.replace('pkl', 'msa'), 'tar', base_folder + 'distillation_dataset/pkl/pkl_10')
+os.system(f'aws s3 cp s3://AF_data/distillation_dataset/msa/msa_0 {base_folder}distillation_dataset/msa/msa_0 --recursive')
+
+shutil.make_archive(f'{base_folder}distillation_dataset/msa/msa_0.tar', 'tar',  f'{base_folder}distillation_dataset/msa/msa_0')
