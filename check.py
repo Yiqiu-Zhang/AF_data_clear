7e5a5aa6562af685c8c933e1ec2ff59a74e3b04c
Yiqiu-Zhang <ezhang13617@gmail.com>
2022-10-28 15:46:15 +0800

    check what is missing

--- check.py
+++ check.py
@@ -15,7 +15,7 @@ for i in range(256):
 
     with tempfile.TemporaryFile() as temp_pdb:
         proc = subprocess.Popen(['aws', 's3', 'ls',
-                                 f's3://AF_data/true_structure_dataset/msa/pdb_{i}/',
+                                 f's3://AF_data/true_structure_dataset/pdb/pdb_{i}/',
                                  '--recursive',
                                  '--human-readable',
                                  '--summarize'], stdout=temp_pdb)
