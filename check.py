bc912ffe499f5ce9fae858c9832bd452cd0790a7
Yiqiu-Zhang <ezhang13617@gmail.com>
2022-10-28 15:33:10 +0800

    check what is missing

--- check.py
+++ check.py
@@ -1,13 +1,36 @@
 import subprocess
 import tempfile
+for i in range(256):
+    with tempfile.TemporaryFile() as temp_msa:
+        proc = subprocess.Popen(['aws', 's3', 'ls',
+                                 f's3://AF_data/true_structure_dataset/msa/msa_{i}/',
+                                 '--recursive',
+                                 '--human-readable',
+                                 '--summarize'], stdout=temp_msa)
+        proc.wait()
+        temp_msa.seek(0)
+        number_of_msa = temp_msa.readlines()[-2]
 
-with tempfile.TemporaryFile() as tempf:
-    proc = subprocess.Popen(['aws', 's3', 'ls',
-                             's3://AF_data/true_structure_dataset/msa/msa_0/',
-                             '--recursive',
-                             '--human-readable',
-                             '--summarize'], stdout=tempf)
-    proc.wait()
-    tempf.seek(0)
-    print (tempf.read())
 
+
+    with tempfile.TemporaryFile() as temp_pdb:
+        proc = subprocess.Popen(['aws', 's3', 'ls',
+                                 f's3://AF_data/true_structure_dataset/msa/pdb_{i}/',
+                                 '--recursive',
+                                 '--human-readable',
+                                 '--summarize'], stdout=temp_pdb)
+        proc.wait()
+        temp_pdb.seek(0)
+        number_of_pdb = temp_pdb.readlines()[-2]
+        if number_of_msa != number_of_pdb:
+            '''
+            msa_list = temp_msa.readlines()[:-2]
+            pdb_list = temp_pdb.readlines()[:-2]
+            for pdb in pdb_list:
+                if pdb.replace('.','/').split('/')[-2] not in msa_list:
+            '''
+            print(f'difference in pdb_{i}')
+            print(number_of_pdb-number_of_msa)
+
+        temp_pdb.close()
+        temp_msa.close()
