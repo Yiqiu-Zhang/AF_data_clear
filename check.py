65ea9162591b4e092a0473346454a43536b285ba
Yiqiu-Zhang <ezhang13617@gmail.com>
2022-10-28 14:40:27 +0800

    check what is missing

---
+++ check.py
@@ -0,0 +1,10 @@
+import subprocess
+import tempfile
+
+with tempfile.TemporaryFile() as tempf:
+    proc = subprocess.Popen(['aws', 's3', 'ls',
+                             's3://AF_data/true_structure_dataset/msa/msa_0'], stdout=tempf)
+    proc.wait()
+    tempf.seek(0)
+    print (tempf.read())
+
